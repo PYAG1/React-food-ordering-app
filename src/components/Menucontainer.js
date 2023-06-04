e88388de08b7c573051e96461e116527a2db229d
PYAG1 <96929032+PYAG1@users.noreply.github.com>
2023-06-04 01:42:55 +0000

    commit

--- src/components/Menucontainer.js
+++ src/components/Menucontainer.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Button } from '@mui/material'
-import { FaArrowLeft, FaTimes } from 'react-icons/fa'
+import { FaArrowLeft } from 'react-icons/fa'
 import Extrainfo from './extrainfo'
 import { useDispatch, useSelector } from 'react-redux'
 import { cartActions } from '../store/cartslice'
